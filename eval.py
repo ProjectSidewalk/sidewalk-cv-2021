20c6edb862f240f473420dac19b761367fe8ffca
ThatOneGoat <mcduan01@gmail.com>
2021-11-16 22:23:05 -0800

    eval commented out for now since we are just training

--- eval.py
+++ eval.py
@@ -1,7 +1,7 @@
 # make sure to evaluate on *_pretrained if loading pretrained model
-test_accuracy, test_loss, cm = evaluate(resnet50_pretrained, loss_func, test_dataloader, True)
-print("Test accuracy for ResNet as FT: ", test_accuracy)
-print("Test loss for ResNet as FT: ", test_loss)
-# print(train_set.img_labels[:50])
-if cm is not None:
-  plot_confusion_matrix(cm, ["null", "curb ramp", "missing ramp", "obstruction", "sfc problem"])
+# test_accuracy, test_loss, cm = evaluate(resnet50_pretrained, loss_func, test_dataloader, True)
+# print("Test accuracy for ResNet as FT: ", test_accuracy)
+# print("Test loss for ResNet as FT: ", test_loss)
+# # print(train_set.img_labels[:50])
+# if cm is not None:
+#   plot_confusion_matrix(cm, ["null", "curb ramp", "missing ramp", "obstruction", "sfc problem"])
