35a9419f85264ef8a4396b0c247c4e27b879fa44
ThatOneGoat <mcduan01@gmail.com>
2021-11-17 16:26:54 -0800

    added dataset csv splitter

--- utils/dataset_splitter.py
+++ utils/dataset_splitter.py
@@ -1,7 +1,35 @@
-import copy
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
-import os
 import pandas as pd
-import itertools
+
+COMPLETE_DATASET_CSV_PATH = "../datasets/crop_info.csv"
+NON_NULL_CROP_DATASET_CSV_PATH = "../datasets/non_null_crop_info.csv"
+SUBSET_CSV_PATH = "../datasets/subset_crop_info.csv"
+
+# read CSV into a Pandas Dataframe
+complete_dataset_df = pd.read_csv(COMPLETE_DATASET_CSV_PATH)
+print(complete_dataset_df.head(30))
+
+# create CSV with just null crops
+# creates a subset df of all rows with 'label_type' not 0
+non_null_crops = complete_dataset_df.loc[complete_dataset_df['label_type'] != 0]
+print(non_null_crops.head(30))
+
+# writes non-null crop df to a csv at specified path without the indexing column
+non_null_crops.to_csv(NON_NULL_CROP_DATASET_CSV_PATH, index=False)
+
+# create null crop dataframe
+# creates a subset df of all rows with 'label_type' 0
+null_crops = complete_dataset_df.loc[complete_dataset_df['label_type'] == 0]
+print(null_crops.head(30))
+
+# get null crop count
+n = len(null_crops.index)
+print(n)
+
+# create Subset CSV of all non-null crops and the first m of the null crops
+m = 0
+m_null_crops = null_crops.head(m)
+
+subset_df = pd.concat([non_null_crops, m_null_crops])
+print(subset_df.head(30))
+
+subset_df.to_csv(SUBSET_CSV_PATH, index=False)
