1457e39c89524eae45716b5dddcf4f2c97ef8eb8
Logan Milandin <mi1andin@uw.edu>
2022-01-29 13:06:09 -0800

    bug fixes

--- visualization_utils/plot_results_vs_metric.py
+++ visualization_utils/plot_results_vs_metric.py
@@ -27,40 +27,40 @@ final_recalls = [[], [], [], [], []]
 
 
 for name in SESSION_NAMES:
-    precisions[name]
-    TRAIN_SAVE_PATH = "../datasets/" + SESSION_NAME + ".pt"
+    TRAIN_SAVE_PATH = "../datasets/" + name + ".pt"
 
     results = torch.load(TRAIN_SAVE_PATH)
     metrics = results['metrics']
     epochs = results['epoch'] + 1  # epochs are 0-indexed in checkpoint
 
     val_accuracy, train_accuracy = metrics['accuracy_validation'], metrics['accuracy_train']
-    max_train_accuracies.append(train_accuracy[-1])
-    max_val_accuracies.append(val_accuracy[-1])
+    final_train_accuracies.append(train_accuracy[-1])
+    final_val_accuracies.append(val_accuracy[-1])
 
-    final_precisions = metrics['precision_validation'][-1]
-    final_recalls = metrics['recall_validation'][-1]
-    for precision, i in enumerate(final_precisions):
-        precisions[i].append(precision)
-    for recall, i in enumerate(final_recalls):
-        recalls[i].append(recall)
+    for i, precision in enumerate(metrics['precision_validation'][-1]):
+        final_precisions[i].append(precision)
+    for i, recall in enumerate(metrics['recall_validation'][-1]):
+        final_recalls[i].append(recall)
 
 figure(figsize=(16, 12))
 plt.plot(sizes, final_train_accuracies, label="train")
 plt.plot(sizes, final_val_accuracies, label="validation")
 plt.title("accuracy vs dataset size")
+plt.legend(prop={'size': 16})
 plt.savefig(f"{VISUALIZATIONS_PATH}/accuracy_vs_size")
 
 figure(figsize=(16, 12))
 for i in range(5):
     plt.plot(sizes, final_precisions[i], label=label_types[i])
 plt.title("precisions vs dataset size")
+plt.legend(prop={'size': 16})
 plt.savefig(f"{VISUALIZATIONS_PATH}/precision_vs_size")
 
 figure(figsize=(16, 12))
 for i in range(5):
     plt.plot(sizes, final_recalls[i], label=label_types[i])
 plt.title("recalls vs dataset size")
+plt.legend(prop={'size': 16})
 plt.savefig(f"{VISUALIZATIONS_PATH}/recall_vs_size")
 
 
