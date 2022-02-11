622e573791bdf1e4a7078ef0b89660cc2fac7696
Logan Milandin <mi1andin@uw.edu>
2022-02-10 22:48:18 -0800

    combines with hrnet training

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
+import citysurfaces.network.hrnetv2 as hrnetv2
 from datatypes.dataset import SidewalkCropsDataset
 from utils.training_utils import get_pretrained_model, load_training_checkpoint, train
 from torch.optim import lr_scheduler
@@ -12,16 +13,19 @@ from torchvision import transforms
 BASE_PATH = "./datasets/"
 
 # name of model architecture
-MODEL_NAME = "MODEL NAME HERE"
+MODEL_NAME = "efficientnet"
 
 # number of output classes
-NUM_CLASSES = 5  # (1,2,3,4) for label types, 0 for null crops
+NUM_CLASSES = "NUM CLASSES"  # (1,2,3,4) for label types, 0 for null crops
 
 # name of training session for saving purposes
-TRAIN_SESSION_NAME = "TRAIN SESSION NAME HERE"
+TRAIN_SESSION_NAME = "MODEL NAME HERE"
+
+# save path for model
+CHECKPOINT_SAVE_PATH = BASE_PATH + TRAIN_SESSION_NAME + ".pt"
 
 # for zoom testing
-CROP_SIZE = 1500
+CROP_SIZE = "CROP SIZE HERE"
 
 # check for GPU
 if torch.cuda.is_available():  
@@ -33,7 +37,10 @@ print(device)
 
 # =================================================================================================
 # setup model for fine tuning
-model, input_size = get_pretrained_model(MODEL_NAME, NUM_CLASSES, False)
+if MODEL_NAME == "hrnet":
+  model, input_size = hrnetv2.load_hrnet_checkpoint(CHECKPOINT_SAVE_PATH, NUM_CLASSES, True), 224
+else:
+  model, input_size = get_pretrained_model(MODEL_NAME, NUM_CLASSES, False)
 model.to(device)
 
 lr = 0.01
@@ -51,8 +58,7 @@ weight_decay = 1e-6
 # add normalized_weights_tensor as input to loss_func if weighted loss is desired
 loss_func = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay) # torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-6)
-scheduler = lr_scheduler.CyclicLR(optimizer, base_lr=1e-6, max_lr=lr, step_size_up = 2500, mode='triangular2') # lr_scheduler.StepLR(optimizer, 10, gamma=0.3)
-checkpoint_save_path = BASE_PATH + TRAIN_SESSION_NAME + ".pt"
+scheduler = lr_scheduler.StepLR(optimizer, 10, gamma=0.5) #lr_scheduler.CyclicLR(optimizer, base_lr=1e-6, max_lr=lr, step_size_up = 2500, mode='triangular2')
 
 # =================================================================================================
 # load train datasets
@@ -65,13 +71,13 @@ image_transform = transforms.Compose([
 ])
 
 # having issues with CUDA running out of memory, so lowering batch size
-batch_size = 8
+batch_size = 12
 
-train_labels_csv_path = BASE_PATH + "train_crop_info.csv"
+train_labels_csv_path = BASE_PATH + "CSV PATH HERE"
 train_img_dir = BASE_PATH + "train_crops/"
 
 # load our custom train/val sidewalk crops dataset
-train_val_dataset = SidewalkCropsDataset(train_labels_csv_path, train_img_dir, image_transform)
+train_val_dataset = SidewalkCropsDataset(train_labels_csv_path, train_img_dir, image_transform, eval=False)
 
 # partition train dataset into 80/20 split for train/validation
 k = .8
@@ -89,6 +95,7 @@ val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size,
 print(len(train_dataset))
 print(len(val_dataset))
 
+
 # =================================================================================================
 # train for n epochs
 epochs = 50
@@ -96,9 +103,9 @@ dataLoaders = {
   "training": train_dataloader,
   "validation": val_dataloader
 }
-metrics, last_epoch = load_training_checkpoint(model, checkpoint_save_path, optimizer, scheduler)
+metrics, last_epoch = load_training_checkpoint(model, , optimizer, scheduler)
 print("next epoch: " + str(last_epoch + 1))
 print("resuming training...\n")
 
 train(model, NUM_CLASSES, (MODEL_NAME == "inception"), optimizer, scheduler, loss_func, epochs, dataLoaders,
-      checkpoint_save_path, metrics, last_epoch + 1, device)
+      , metrics, last_epoch + 1, device)
