183df3187a959b5eddbce2c03205f656699443e8
ThatOneGoat <mcduan01@gmail.com>
2022-05-26 00:06:40 -0700

    Updated experiment scripts

--- experiments/experiment3_1.sh
+++ experiments/experiment3_1.sh
@@ -19,9 +19,9 @@ model_save_folder="../models/"
 # save path the visualizations
 visualizations_path="../visualizations/"
 # number of epochs for training
-num_epochs="15"
+num_epochs="10"
 # crop size
-crop_size="1250"
+crop_size="1500"
 # number of plots for mistake visualization
 num_plots="5"
 
@@ -61,16 +61,16 @@ for label in {1..4}; do
   wc -l $csv_base_path/"tmp/all_cities/train_set"$label".csv"
 
   # train model on combined train set
-  python ../train.py ${experiment}_${model_name}_$label $image_base_path $csv_base_path/"tmp/all_cities/train_set"$label".csv" $model_name $model_save_folder/$experiment $num_epochs $crop_size
+  python ../train.py ${experiment}_${model_name}_$label $image_base_path/ $csv_base_path/"tmp/all_cities/train_set"$label".csv" $model_name $model_save_folder/$experiment $num_epochs $crop_size
 
   for city in ${cities[@]}; do
     echo "testing label "$label" classifier on "$city"..."
     # evaluate model on each city
-    python ../eval.py ${experiment}_${model_name}_$label $image_base_path $csv_base_path/"tmp/"$city/"test_set"$label".csv" $model_name $model_save_folder/$experiment $visualizations_path/$experiment/$city $crop_size
+    python ../eval.py ${experiment}_${model_name}_$label $image_base_path/ $csv_base_path/"tmp/"$city/"test_set"$label".csv" $model_name $model_save_folder/$experiment $visualizations_path/$experiment/$city $crop_size
     # analyze results
     python ../visualization_utils/analyze_results.py ${experiment}_${model_name}_$label $model_save_folder/$experiment $visualizations_path/$experiment/$city
     # visualize mistakes
-    python ../visualization_utils/visualize_mistakes.py ${experiment}_${model_name}_$label $image_base_path $visualizations_path/$experiment/$city $crop_size $num_plots
+    python ../visualization_utils/visualize_mistakes.py ${experiment}_${model_name}_$label $image_base_path/ $visualizations_path/$experiment/$city $crop_size $num_plots
   done
 done
 
