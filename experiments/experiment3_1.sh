438ef5f15edca9d1333dd313cac02403d2695c3f
Logan Milandin <mi1andin@uw.edu>
2022-05-26 23:30:21 -0700

    cleans up viz panels

--- experiments/experiment3_1.sh
+++ experiments/experiment3_1.sh
@@ -4,6 +4,8 @@ echo "Starting Experiment 3.1"
 experiment="3_1"
 # city names
 cities=("seattle")
+# label types
+labels=("curb_ramp", "missing_curb_ramp", "obstacle", "surface_problem")
 # path to train/test CSV data
 csv_base_path="../datasets/"
 # train set CSV filename
@@ -30,8 +32,8 @@ echo "initializing..."
 for city in ${cities[@]}; do
   mkdir -p $csv_base_path/"tmp/"$city
   for label in {1..4}; do
-    python ../utils/dataset_creator.py "binarize" $csv_base_path/$city/${city}_$train_set_csv $label $csv_base_path/"tmp/"$city/"train_set"$label".csv"
-    python ../utils/dataset_creator.py "binarize" $csv_base_path/$city/${city}_$test_set_csv $label $csv_base_path/"tmp/"$city/"test_set"$label".csv"
+    python ../utils/dataset_creator.py "binarize" $csv_base_path/$city/${city}_$train_set_csv $label $csv_base_path/"tmp/"$city/"train_set_"${labels[$label]}".csv"
+    python ../utils/dataset_creator.py "binarize" $csv_base_path/$city/${city}_$test_set_csv $label $csv_base_path/"tmp/"$city/"test_set_"{labels[$label]}".csv"
   done
 done
 
@@ -42,35 +44,29 @@ for city in ${cities[@]}; do
   mkdir -p $visualizations_path/$experiment/$city
 done
 
-for label in {1..4}; do
+for label in "curb_ramp" "missing_curb_ramp" "obstacle" "surface_problem"; do
   echo "training label "$label" classifier on all cities..."
   # compose list of train set csvs to combine
   arguments=""
   for city in ${cities[@]}; do
-    arguments+="$csv_base_path/tmp/$city/train_set$label.csv "
+    arguments+="$csv_base_path/tmp/$city/train_set_"${labels[$label]}".csv "
   done
-  arguments+=$csv_base_path/"tmp/all_cities/train_set"$label".csv"
-
-  # combine train sets for all cities
-  # head -n1 $csv_base_path/"tmp/"${cities[1]}/"train_set"$label".csv" > $csv_base_path/"tmp/all_cities/train_set"$label".csv"
-  # for city in ${cities[@]}; do
-  #   python ../utils/dataset_creator.py "combine" $csv_base_path/"tmp/all_cities/train_set"$label".csv" $csv_base_path/"tmp/"$city/"train_set"$label".csv" $csv_base_path/"tmp/all_cities/train_set"$label".csv"
-  # done
+  arguments+=$csv_base_path/"tmp/all_cities/train_set_"${labels[$label]}".csv"
 
   python ../utils/dataset_creator.py "combine" $arguments
-  wc -l $csv_base_path/"tmp/all_cities/train_set"$label".csv"
+  wc -l $csv_base_path/"tmp/all_cities/train_set_"${labels[$label]}".csv"
 
   # train model on combined train set
-  python ../train.py ${experiment}_${model_name}_$label $image_base_path $csv_base_path/"tmp/all_cities/train_set"$label".csv" $model_name $model_save_folder/$experiment $num_epochs $crop_size
+  python ../train.py ${experiment}_${model_name}_${labels[$label]} $image_base_path $csv_base_path/"tmp/all_cities/train_set_"${labels[$label]}".csv" $model_name $model_save_folder/$experiment $num_epochs $crop_size
 
   for city in ${cities[@]}; do
-    echo "testing label "$label" classifier on "$city"..."
+    echo "testing label "${labels[$label]}" classifier on "$city"..."
     # evaluate model on each city
-    python ../eval.py ${experiment}_${model_name}_$label $image_base_path $csv_base_path/"tmp/"$city/"test_set"$label".csv" $model_name $model_save_folder/$experiment $visualizations_path/$experiment/$city $crop_size
+    python ../eval.py ${experiment}_${model_name}_${labels[$label]} $image_base_path $csv_base_path/"tmp/"$city/"test_set_"${labels[$label]}".csv" $model_name $model_save_folder/$experiment $visualizations_path/$experiment/$city $crop_size
     # analyze results
-    python ../visualization_utils/analyze_results.py ${experiment}_${model_name}_$label $model_save_folder/$experiment $visualizations_path/$experiment/$city
+    python ../visualization_utils/analyze_results.py ${experiment}_${model_name}_${labels[$label]} $model_save_folder/$experiment $visualizations_path/$experiment/$city
     # visualize mistakes
-    python ../visualization_utils/visualize_mistakes.py ${experiment}_${model_name}_$label $image_base_path $visualizations_path/$experiment/$city $crop_size $num_plots
+    python ../visualization_utils/visualize_mistakes.py ${experiment}_${model_name}_${labels[$label]} $image_base_path $visualizations_path/$experiment/$city $crop_size $num_plots
   done
 done
 
